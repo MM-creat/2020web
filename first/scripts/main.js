299221dcddd4c9c8c7c52d22ff1722f9f4c4f88c
MM-creat <75403362+MM-creat@users.noreply.github.com>
2020-12-10 14:35:22 +0900

    Update main.js

--- first/scripts/main.js
+++ first/scripts/main.js
@@ -1,14 +1,14 @@
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
-myButton.onclick = function() {
-     setUserName();
-}
-
 function setUserName() {
    let myName = prompt('あなたの名前を入力してください。');
    localStorage.setItem('name', myName);
    myHeading.textContent = 'Mozilla はすばらしいよ、' + myName;
+}
+
+myButton.onclick = function() {
+     setUserName();
 }
    
 
